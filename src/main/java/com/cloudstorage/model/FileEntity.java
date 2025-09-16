5f3b60c7b5e91d8dc48511a6fdf50ca17b7983cf
Sasha125a <pushkatank2@gmail.com>
2025-09-16 18:15:42 +0400

    Update FileEntity.java

--- src/main/java/com/cloudstorage/model/FileEntity.java
+++ src/main/java/com/cloudstorage/model/FileEntity.java
@@ -25,26 +25,3 @@ public class FileEntity {
     
     // Конструкторы, геттеры и сеттеры
 }
-
-// User.java
-package com.cloudstorage.model;
-
-import jakarta.persistence.*;
-import java.util.List;
-
-@Entity
-@Table(name = "users")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private String username;
-    private String email;
-    private String password;
-    
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-    private List<FileEntity> files;
-    
-    // Конструкторы, геттеры и сеттеры
-}
